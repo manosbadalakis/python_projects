64706b4f92116d3d6e150fc8275285feb0ed526a
dbada <manosbadalakis@gmail.com>
2021-07-05 16:33:55 +0300

    @Manos Badalakis

---
+++ main.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2021. Manos E. Badalakis. All Rights Reserved.
+print("Finally I Found A Way To Communicate With You")
+
+# exercise ------------------- ΑΣΚΗΣΗ -------------------
+# TODO 1. Εκτυπόστε τον χαιρετισμό "Hello World!"
+# TODO 2. Εκτυπόστε τον κώδικα για να δείτε τι κάνει!
