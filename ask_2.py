574bd1cb369e6ccf8305ef877d54e0f358d479c2
dbada <manosbadalakis@gmail.com>
2021-09-20 16:09:31 +0300

    @Manos Badalakis

---
+++ ask_2.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2021. Manos. Bantalakis All Right Reserved.
+
+# example ------------------- ΠΑΡΑΔΕΙΓΜΑ ---------------------
+# Η παρακάτω μεταβλητή ακοποιεί τον χρόνο πτήσης ενός αεροσκάφους
+airplane_fight_time = 0
+# aireplane_fight_time_in_realtime = old_function()
